dc2dd244efc340250e7d786d658cf6395d5b2260
Ferran Lao <flao@telecomboutique.com>
2020-01-15 10:52:54 +0100

    userCheck bug fixed

--- server/helpers/validations/validUser.js
+++ server/helpers/validations/validUser.js
@@ -1,6 +1,6 @@
 import validator from "validator"
 
-export default ({email,name})=>{
-    if(!validator.isEmail(email)||!validator.isAlpha(name))return false
+export default ({ email, name, password }) => {
+    if (!email || !name || !password || !validator.isEmail(email) || !validator.isAlpha(name)) return false
     return true
 }
